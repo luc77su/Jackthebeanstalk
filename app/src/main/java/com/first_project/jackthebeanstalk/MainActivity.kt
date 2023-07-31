abcfa2f07a89384f569570bfe9cfe7ab4d0c5da5
mark <polysg@asleep.ai>
2023-07-31 22:48:37 +0900

    Initial commit

---
+++ app/src/main/java/com/first_project/jackthebeanstalk/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.first_project.jackthebeanstalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
